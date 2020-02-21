0aec536a42e0cf45d7b7215a41a6deb48e9e9db7
ryogosato <60603711+ryogosato@users.noreply.github.com>
2020-02-21 12:16:13 +0900

    ユーザー管理機能追加

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true
+
+  has_many :groups_users
+  has_many :groups, through: :groups_users
+  has_many :messages
+end
