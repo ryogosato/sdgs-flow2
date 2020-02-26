e31de29cb6803d651fde174c31b638632e578f50
ryogosato <60603711+ryogosato@users.noreply.github.com>
2020-02-26 09:02:02 +0900

    form_seve

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,8 +1,22 @@
 class MessagesController < ApplicationController
+  before_action :set_group
+
   def index
-    @groups = Group.find(params[:group_id])
+    @message = Message.new
   end
 
-  def edit
+  def create
+    @groups.messages.create(message_params)
+    redirect_to group_messages_path(@groups)
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
+  end
+
+  def set_group
+    @groups = Group.find(params[:group_id])
   end
 end
