7126e62f06a8a93bd18b7ad4ee00373fce7e6d47
ryogosato <60603711+ryogosato@users.noreply.github.com>
2020-03-03 15:42:21 +0900

    ユーザー編集機能追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name]) # 新規登録時(sign_up時)にnameというキーのパラメーターを追加で許可する
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :email, :password]) 
   end
 end
