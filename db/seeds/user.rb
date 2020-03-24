c33f5a54c05801fc9cd2eb5f2bbffe301a6bebb0
ryogosato <60603711+ryogosato@users.noreply.github.com>
2020-03-24 12:07:19 +0900

    Adjustment name

--- db/seeds/user.rb
+++ db/seeds/user.rb
@@ -1,48 +1,19 @@
 User.create!(
   [
     {
-      nickname: 'taro',
-      email: 't1@taro.com',
-      first_name: '太郎',
-      last_name: '山田',
-      furigana_first: 'タロウ',
-      furigana_last: 'ヤマダ',
-      birthdate: '2010-07-11',
-      tell: '00000000000',
-      password: "taro123"
+      name: 'ryogo',
+      email: 'ryogo@ryogo.com',
+      password: "ryogo8888"
     },
     {
-      nickname: 'jiro',
+      name: 'jiro',
       email: 'te2@jiro.com',
-      first_name: '二郎',
-      last_name: '山田',
-      furigana_first: 'ジロウ',
-      furigana_last: 'ヤマダ',
-      birthdate: '2010-06-11',
-      tell: '00000000000',
       password: "jiro123"
     },
     {
-      nickname: 'ichiro',
+      name: 'ichiro',
       email: 'tes3@ichiro.com',
-      first_name: '一郎',
-      last_name: '山田',
-      furigana_first: 'イチロウ',
-      furigana_last: 'ヤマダ',
-      birthdate: '2010-08-11',
-      tell: '00000000000',
       password: "ichiro1"
-    },
-    {
-      nickname: 'saburo',
-      email: 'test4@saburo.com',
-      first_name: '三郎',
-      last_name: '山田',
-      furigana_first: 'サブロウ',
-      furigana_last: 'ヤマダ',
-      birthdate: '2010-07-11',
-      tell: '00000000000',
-      password: "saburo1"
     },
   ]
 )
