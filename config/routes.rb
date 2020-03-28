ba0048be80afeeea05c62e957b8296ec33405fd7
ryogosato <60603711+ryogosato@users.noreply.github.com>
2020-03-28 13:08:18 +0900

    edit bottun view

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   root 'tops#index'
   resource :tops, only: :show
   resources :groups, only: [:new, :create, :edit, :update, :show, :destroy] do
-    resources :messages, only: [:index, :create, :edit, :destroy]
+    resources :messages, only: [:index, :create, :destroy]
+    resource :messages, only: :edit
   end
   resources :users, only: :show
 end
