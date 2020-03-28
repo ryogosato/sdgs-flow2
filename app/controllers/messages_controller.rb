23f9fd027a2a7343f329cb95296bfbd1066f0a2e
ryogosato <60603711+ryogosato@users.noreply.github.com>
2020-03-28 13:17:29 +0900

    edit return

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -26,7 +26,7 @@ class MessagesController < ApplicationController
   def destroy
     message = Message.find(params[:id])
     message.destroy
-    redirect_to group_messages_path(@group)
+    redirect_to edit_group_messages_path(@group)
   end
 
   private
